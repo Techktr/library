cee10e0112801b0e1608a6cb2e9014f1ef65db2b
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2025-12-30 18:29:52 +1100

    feat: ajout des méthodes de mapping des list

--- src/main/java/com/example/library/mapper/LivreMapper.java
+++ src/main/java/com/example/library/mapper/LivreMapper.java
@@ -3,17 +3,28 @@ package com.example.library.mapper;
 import com.example.library.dto.LivreDTO;
 import com.example.library.model.Livre;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class LivreMapper {
 
     public LivreMapper() {}
 
-    public LivreDTO map(Livre livre) {
+    public LivreDTO toDto(Livre livre) {
         return new LivreDTO(livre.getNom(), livre.getAuteur(), livre.getIsbn(), livre.getPublication(), livre.getGenre(), livre.getResume(), livre.isStatut());
     }
 
-    public Livre map(LivreDTO livreDTO) {
+    public Livre toEntity(LivreDTO livreDTO) {
         return new Livre(livreDTO.getNom(), livreDTO.getAuteur(), livreDTO.getIsbn(), livreDTO.getPublication(), livreDTO.getGenre(), livreDTO.getResume(), livreDTO.isStatut());
+    }
+
+    public List<LivreDTO> toDtos(List<Livre> livreList) {
+        return livreList.stream().map(this::toDto).toList();
+    }
+
+    public List<Livre> toEntities(List<LivreDTO> livreDTOList) {
+        return livreDTOList.stream().map(this::toEntity).toList();
     }
 
 }
